535ee2c09ff86bffb58786302ed8f817cee8426a
Lu Li <lulu.li1989@gmail.com>
2019-04-11 12:30:14 -0700

    1) Fix the script to be up to date 2) Parse more fields needed

--- sephora_spider.py
+++ sephora_spider.py
@@ -1,5 +1,5 @@
 from scrapy import Spider, Request
-from sephora.items import SephoraItem
+from items import SephoraItem
 import re
 import pandas as pd
 import json
@@ -7,6 +7,7 @@ import math
 import time
 
 n_count_tot = 0
+product_count_tot = 0
 
 class SephoraSpider(Spider):
 
@@ -38,24 +39,27 @@ class SephoraSpider(Spider):
 			yield Request(url, callback=self.parse_product)
 
 	def parse_product(self, response):
-		#time.sleep(0.5)	
-		dictionary = response.xpath('//script[@id="searchResult"]/text()').extract()
+		#time.sleep(0.5)
+		dictionary = response.xpath('//script[@id="linkJSON"]').extract()
 		dictionary = re.findall('"products":\[(.*?)\]', dictionary[0])[0]
 		
-		product_urls = re.findall('"product_url":"(.*?)",', dictionary)
-		product_names = re.findall('"display_name":"(.*?)",', dictionary)
-		product_ids = re.findall('"id":"(.*?)",', dictionary)
+		product_urls = re.findall('"targetUrl":"(.*?)",', dictionary)
+		product_names = re.findall('"displayName":"(.*?)",', dictionary)
+		product_ids = re.findall('"productId":"(.*?)",', dictionary)
 		ratings = re.findall('"rating":(.*?),', dictionary)
-		brand_names = re.findall('"brand_name":"(.*?)",', dictionary)
-		#list_prices = re.findall('"list_price":(.*?),', dictionary)
+		brand_names = re.findall('"brandName":"(.*?)",', dictionary)
+		list_prices = re.findall('"listPrice":(.*?),', dictionary)
+		# sale_prices
 
 		links2 = ["https://www.sephora.com" + link for link in product_urls]
-
 		if len(product_urls)!=len(ratings)!=len(brand_names):
 			print('Number of products do not match with ratings')
 
+		global product_count_tot
+		product_count_tot = len(links2)
+
 		product_df = pd.DataFrame({'links2': links2,'product_names': product_names,'p_id': product_ids, 
-			'ratings': ratings,'brand_names': brand_names})
+			'ratings': ratings,'brand_names': brand_names, 'p_price': list_prices})
 
 		print (product_df.head())
 		print (list(product_df.index))
@@ -65,14 +69,16 @@ class SephoraSpider(Spider):
 			p_id = product_df.loc[n, 'p_id']
 			p_star = product_df.loc[n, 'ratings']
 			brand_name = product_df.loc[n, 'brand_names']
+			p_price = product_df.loc[n, 'p_price']
 
 			print (product_df.loc[n,'links2'])
 
-			#if n>0:
-				#time.sleep(20)
-
+			if n>0:
+				time.sleep(1)
+				
 			yield Request(product_df.loc[n,'links2'], callback=self.parse_detail,
-				meta={'product': product, 'p_id':p_id, 'p_star':p_star, 'brand_name':brand_name,})
+				meta={'product': product, 'p_id':p_id, 'p_star':p_star, 'brand_name':brand_name,
+				      'p_price': p_price})
 
 	def parse_detail(self, response):
 		#time.sleep(0.5)
@@ -82,19 +88,16 @@ class SephoraSpider(Spider):
 		p_id = response.meta['p_id']
 		p_star = response.meta['p_star']
 		brand_name = response.meta['brand_name']
+		p_price = response.meta['p_price']
 
-		p_category = response.xpath('//a[@class="css-u2mtre"]/text()').extract_first()
+		p_categories = response.xpath('//a[@class="css-1ylrown "]/text()').extract()
+		p_categories += response.xpath('//h1[@class="css-bnsadm "]/text()').extract()
 
-		try:
-			p_price = response.xpath('//div[@class="css-18suhml"]/text()').extract()
-			p_price = p_price[0]
-		except:
-			p_price = None
-
-		p_num_reviews = response.xpath('//span[@class="css-1dz7b4e"]/text()').extract()
+		p_num_reviews = response.xpath('//a[@data-comp="RatingsSummary Flex Box"]/span/text()').extract()
 		p_num_reviews = p_num_reviews[0]
 		p_num_reviews = p_num_reviews.replace('s', '')
 		p_num_reviews = p_num_reviews.replace(' review', '')
+		p_num_reviews = p_num_reviews.replace('K', '000')
 		p_num_reviews = int(p_num_reviews)
 
 		print ('Number of reviews: {}'.format(p_num_reviews))
@@ -107,18 +110,18 @@ class SephoraSpider(Spider):
 		#up_range = [x*30 for x in list(range(1,max_n+1))]
 
 		links3 = ['https://api.bazaarvoice.com/data/reviews.json?Filter=ProductId%3A' +
-			p_id + '&Sort=Helpfulness%3Adesc&Limit=' + 
-			'{}&Offset={}&Include=Products%2CComments&'.format(min(int(p_num_reviews), int(100)), 0) +
+			p_id + '&Sort=SubmissionTime%3Adesc&Limit=' + 
+			'{}&Offset={}&Include=Products%2CComments&'.format(min(int(p_num_reviews), 100), 0) +
 			'Stats=Reviews&passkey=rwbw526r2e7spptqd2qzbkp7&apiversion=5.4']
 
 		for url in links3:
-			#time.sleep(0.5)
+			time.sleep(0.5)
 			yield Request(url, callback=self.parse_reviews,
 				meta={'product': product, 'p_id':p_id, 'p_star':p_star, 'brand_name':brand_name,
-				'p_category':p_category, 'p_num_reviews':p_num_reviews, 'p_price':p_price})
+				'p_categories':p_categories, 'p_num_reviews':p_num_reviews, 'p_price':p_price})
 
 	def parse_reviews(self, response):
-		#time.sleep(0.5)
+		time.sleep(0.5)
 		print('parse_reviews')
 
 		product = response.meta['product']
@@ -126,7 +129,7 @@ class SephoraSpider(Spider):
 		p_star = response.meta['p_star']
 		brand_name = response.meta['brand_name']
 		p_price = response.meta['p_price']
-		p_category = response.meta['p_category']
+		p_categories = response.meta['p_categories']
 		p_num_reviews = response.meta['p_num_reviews']
 
 		data = json.loads(response.text)
@@ -139,10 +142,9 @@ class SephoraSpider(Spider):
 		#create code here which arranges the data from the json dictionary into a dataframe
 
 		n_count = 0
-		global n_count_tot
-
+		global n_count_tot, product_count_tot
+		
 		for review in reviews:
-			
 			try:
 				reviewer = review['UserNickname']
 			except:
@@ -181,19 +183,44 @@ class SephoraSpider(Spider):
 			except:
 				r_review = None
 
+			try:
+				r_last_modified_time = review['LastModificationTime']
+			except:
+				r_last_modified_time = None
+
+			try:
+				r_negative_count = review['TotalNegativeFeedbackCount']
+			except:
+				r_negative_count = None
+
+			try:
+				r_positive_count = review['TotalPositiveFeedbackCount']
+			except:
+				r_positive_count = None
+
+			try:
+				r_userlocation = review['UserLocation']
+			except:
+				r_userlocation = None
+
+			try:
+				r_title = review['Title']
+			except:
+				r_title = None
+
 			#need to create an error handler for empty data for reviews
 
-			print ('BRAND: {} PRODUCT: {}'.format(brand_name, product))
-			print ('ID: {} STARS: {}'.format(reviewer, r_star))
-			print ('='*50)
-
+			# print ('BRAND: {} PRODUCT: {}'.format(brand_name, product))
+			# print ('ID: {} STARS: {}'.format(reviewer, r_star))
+			# print ('='*50)
+			
 			item = SephoraItem()
 			item['product'] = product
 			item['p_id'] = p_id
 			item['p_star'] = p_star
 			item['brand_name'] = brand_name
 			item['p_price'] = p_price
-			item['p_category'] = p_category
+			item['p_categories'] = p_categories
 			item['p_num_reviews'] = p_num_reviews 
 
     		#all of these needs to be taken from the reviews list/dictionary
@@ -206,6 +233,11 @@ class SephoraSpider(Spider):
 			item['r_skintype'] = r_skintype
 			item['r_skinconcerns'] = r_skinconcerns
 			item['r_review'] = r_review
+			item['r_last_modified_time'] = r_last_modified_time
+			item['r_negative_count'] = r_negative_count
+			item['r_positive_count'] = r_positive_count
+			item['r_userlocation'] = r_userlocation
+			item['r_title'] = r_title
 
 			#time.sleep(0.025)
 			n_count += 1
@@ -213,10 +245,13 @@ class SephoraSpider(Spider):
 
 			yield item
 
+		time.sleep(1)			
 		print ('='*50)
 		print ('TOTAL NUMBER OF REVIEWS: {}'.format(int(p_num_reviews)))
-		print ('NUMBER OF REVIEWS TO BE PULLED: {}'.format(len(reviews)))
+		print ('TOTAL NUMBER PULLED FOR PRODUCT: {}'.format(len(reviews)))
 		print ('ACTUAL NUMBER PULLED {}'.format(n_count))
 		print ('TOTAL NUMBER PULLED {}'.format(n_count_tot))
+		print ('TOTAL NUMBER PRODUCTS {}'.format(product_count_tot))
 		print ('='*50)
+		time.sleep(1)
 
