a6eefc6d88ffdabf7158234e155bb3c9187a5cb5
chenyaoli19 <chenyaoli19@gmail.com>
2020-09-03 09:01:46 -0700

    get first 262 influencer contact info

--- instagram_like_bot.py
+++ instagram_like_bot.py
@@ -6,6 +6,10 @@ import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+# binary = FirefoxBinary('/Applications/Firefox.app/Contents/MacOS/firefox-bin')
+# browser = webdriver.Firefox(firefox_binary=binary)
 
 prev_user_list = [line.rstrip('\n') for line in open('users_advertised_list.txt', 'r')]
 
@@ -23,15 +27,16 @@ comment_dict = {
 
 ad = "Great info!👏 If you care about what you put on your face, we recently launched a search engine for skincare at glowism.com. You could look up skincare products' ingredients information and reviews. You could also create a skin profile to have the information personalized for you. Check it out and let us know what you think 🥰'"
 
+# with webdriver.Firefox(firefox_binary=binary) as webdriver:
 with webdriver.Firefox() as webdriver:
     wait = WebDriverWait(webdriver, 10)
     webdriver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
     sleep(1)
     
     username = webdriver.find_element_by_name('username')
-    username.send_keys('glowism_')
+    username.send_keys('hello@glowism.com')
     password = webdriver.find_element_by_name('password')
-    password.send_keys('t3rat3ra2019')
+    password.send_keys('glowglow2020')
 
     button_login = webdriver.find_element_by_xpath("//button[./div/text()='Log In']")
     button_login.click()
@@ -54,15 +59,16 @@ with webdriver.Firefox() as webdriver:
             try:        
                 username = WebDriverWait(webdriver, 10).until(
                     EC.visibility_of_element_located((By.XPATH, '//a[@class="FPmhX notranslate  nJAzx"]'))).text
+                print(username)
                 button_like = webdriver.find_element_by_xpath('//span[starts-with(@class,"glyphsSpriteHeart__")]')
                 if button_like.get_attribute("aria-label") == "Like":
-                    button_like.click()
+                    # button_like.click()
                     likes += 1
-                    comment_box = webdriver.find_element_by_xpath('//textarea[@class="Ypffh"]')
-                    comment_box.click()
-                    comment = ''
-                    comm_choice = randint(1,10)
-                    comment = comment_dict[comm_choice]
+                    # comment_box = webdriver.find_element_by_xpath('//textarea[@class="Ypffh"]')
+                    # comment_box.click()
+                    # comment = ''
+                    # comm_choice = randint(1,10)
+                    # comment = comment_dict[comm_choice]
 
                     # if username in prev_user_list:
                     #     comm_choice = randint(1,10)
@@ -70,14 +76,14 @@ with webdriver.Firefox() as webdriver:
                     # else:
                     #     comment = ad
                     #     prev_user_list.append(username)
-                    print(comment)
-                    comment_box = webdriver.find_element_by_xpath('//textarea[starts-with(@class, "Ypffh")]')
-                    comment_box.send_keys(comment)
-                    comment_box.send_keys(Keys.ENTER)
+                    # print(comment)
+                    # comment_box = webdriver.find_element_by_xpath('//textarea[starts-with(@class, "Ypffh")]')
+                    # comment_box.send_keys(comment)
+                    # comment_box.send_keys(Keys.ENTER)
                     sleep(randint(20, 30))
                 button_next = webdriver.find_element_by_xpath("//a[./text()='Next']")
                 button_next.click()
-                sleep(randint(1,2))
+                sleep(randint(1, 2))
             except Exception as e:
                 print(e)
                 continue        
